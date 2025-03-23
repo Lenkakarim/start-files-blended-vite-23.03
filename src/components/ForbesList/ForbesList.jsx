d1c4cb5ea0f1d81a7afe5030b12c63ec2a877ab2
Len4ik <karimovaolena@yahoo.com>
2025-03-23 11:13:57 -0400

    add tasks

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,5 +1,25 @@
-const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import styles from "./ForbesList.module.css";
+import ForbesListItem from "../ForbesListItem/ForbesListItem";
+
+const ForbesList = ({ list }) => {
+  return (
+    <div className={styles.board}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>
+          <span className={styles.titleTop}>Forbes</span>
+          <span className={styles.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={styles.list}>
+        {list.map((item) => (
+          <li key={item.id} className={styles.item}>
+            <ForbesListItem data={item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ForbesList;
---
+++ src/helpers/formatDateToNow.js
@@ -0,0 +1,6 @@
+import { formatDistanceToNow } from "date-fns"
+
+export const formatDateToNow = (postedAt) => {
+    return  formatDistanceToNow(new Date (postedAt),{ addSuffix: true })
+    
+}
